5eda7fb9a18f1162903f4d8bae4dec19127e1ac8
Макс Апаков <maksapakov@gmail.com>
2022-09-01 14:08:43 +0300

    ver.2.1

--- src/IOStream.java
+++ src/IOStream.java
@@ -1,17 +1,24 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Array;
 
 public class IOStream {
     public static void print(InputStream inputStream, OutputStream outputStream) throws IOException {
-        byte[] bytes = inputStream.readAllBytes();
+//        byte[] bytes = inputStream.readAllBytes();
+        while (inputStream.available() != 0) {
+            int inRead = inputStream.read();
+            if ((inRead & 1) == 0) {
+                outputStream.write(inRead);
+            }
+        }
+/*
         for (byte b :
                 bytes) {
             if ((b & 1) == 0) {
                 outputStream.write(b);
             }
         }
+*/
         inputStream.close();
         outputStream.close();
     }
